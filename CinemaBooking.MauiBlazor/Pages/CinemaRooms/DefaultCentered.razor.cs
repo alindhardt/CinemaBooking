304da84ad995ce9a40fc77dcfb4f44130a9b6f30
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-09-21 13:00:49 +0200

    Added DefaultCentered room and added to navigation

---
+++ CinemaBooking.MauiBlazor/Pages/CinemaRooms/DefaultCentered.razor.cs
@@ -0,0 +1,30 @@
+﻿using CinemaBooking.MauiBlazor.Data;
+using CinemaBooking.MauiBlazor.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBooking.MauiBlazor.Pages.CinemaRooms
+{
+    public partial class DefaultCentered
+    {
+        [Inject]
+        public ICinemaRoomRepository CinemaRoomRepository { get; set; }
+
+        public int NumberOfSeatsPicked { get; private set; }
+        public List<SeatModel> Seats { get; set; } = new List<SeatModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Seats = await CinemaRoomRepository.GetSeatsFromRoom(1, 1, DateTime.Now);
+        }
+
+        void UpdateNumberOfSeatsPicked()
+        {
+            NumberOfSeatsPicked = Seats.Where(s => s.SeatStatus is SeatStatus.Picked).Count();
+        }
+    }
+}
