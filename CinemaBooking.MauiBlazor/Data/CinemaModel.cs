09c878e52a042330fa2f33bc3ec8928df5d31f43
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-09-24 12:07:18 +0200

    Added Id to CinemaModel

--- CinemaBooking.MauiBlazor/Data/CinemaModel.cs
+++ CinemaBooking.MauiBlazor/Data/CinemaModel.cs
@@ -8,6 +8,7 @@ namespace CinemaBooking.MauiBlazor.Data
 {
     public class CinemaModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
